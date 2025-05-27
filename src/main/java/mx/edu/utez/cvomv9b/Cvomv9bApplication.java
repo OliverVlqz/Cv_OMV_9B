9d2c7a88ff0a28fa57c1b790549553a401ad7b28
OliverVlqz <oliveratom772@gmail.com>
2025-05-27 15:50:31 -0600

    first commit

---
+++ src/main/java/mx/edu/utez/cvomv9b/Cvomv9bApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvomv9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvomv9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvomv9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvomv9b/Cvomv9bApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvomv9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvomv9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
